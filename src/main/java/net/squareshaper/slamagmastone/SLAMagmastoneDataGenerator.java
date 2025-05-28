b7d8b3dc60b169f1a1e0d047a80fc8694e21d448
SquareShaper <anonymoushacker.cheaphacks@gmail.com>
2025-05-28 21:06:41 +0200

    datagen setup

--- src/main/java/net/squareshaper/slamagmastone/SLAMagmastoneDataGenerator.java
+++ src/main/java/net/squareshaper/slamagmastone/SLAMagmastoneDataGenerator.java
@@ -2,10 +2,15 @@ package net.squareshaper.slamagmastone;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.squareshaper.slamagmastone.datagen.ModModelProvider;
+import net.squareshaper.slamagmastone.datagen.ModRecipeProvider;
 
 public class SLAMagmastoneDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
