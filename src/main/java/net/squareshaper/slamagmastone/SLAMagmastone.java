993e8707630f1721ffaa1d58db943108347700b9
SquareShaper <anonymoushacker.cheaphacks@gmail.com>
2025-05-28 20:58:17 +0200

    Initial commit

---
+++ src/main/java/net/squareshaper/slamagmastone/SLAMagmastone.java
@@ -0,0 +1,19 @@
+package net.squareshaper.slamagmastone;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SLAMagmastone implements ModInitializer {
+	public static final String MOD_ID = "sla-magmastone";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static Identifier id(String name){
+		return Identifier.of(MOD_ID, name);
+	}
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
